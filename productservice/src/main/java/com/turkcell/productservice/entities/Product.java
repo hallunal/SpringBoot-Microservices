4360918da8d6f0f4845dfbe665f855b4d5efb216
hallunal <halil.i.unal1997@gmail.com>
2023-10-31 15:57:00 +0300

    Product Service Add Method Added

---
+++ productservice/src/main/java/com/turkcell/productservice/entities/Product.java
@@ -0,0 +1,24 @@
+package com.turkcell.productservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nonapi.io.github.classgraph.json.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Document(value = "products")
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int stock;
+    private String inventoryCode;
+}
---
+++ productservice/src/main/java/com/turkcell/productservice/services/ProductManager.java
@@ -0,0 +1,45 @@
+package com.turkcell.productservice.services;
+
+
+import com.turkcell.productservice.dto.requests.CreateProductRequest;
+import com.turkcell.productservice.dto.responses.CreatedProductResponse;
+import com.turkcell.productservice.entities.Product;
+import com.turkcell.productservice.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductManager implements ProductService{
+    private final ProductRepository productRepository;
+
+    @Override
+    public CreatedProductResponse add(CreateProductRequest request) {
+        Product product = Product
+                .builder()
+                .name(request.getName())
+                .price(request.getPrice())
+                .description(request.getDescription())
+                .stock(request.getStock())
+                .inventoryCode(request.getInventoryCode())
+                .build();
+
+        product = productRepository.save(product);
+
+        CreatedProductResponse response = CreatedProductResponse
+                .builder()
+                .id(product.getId())
+                .name(product.getName())
+                .build();
+        return response;
+    }
+
+    @Override
+    public Product getByInventoryCode(String code) {
+
+        return productRepository.findByInventoryCode(code);
+    }
+}
